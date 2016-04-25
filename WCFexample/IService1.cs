a05ff1c7639c985fcacbc77ed0cdc9b71d73c9f2
GulsumYigit <glsm.ygt@hotmail.com>
2016-04-25 18:06:14 +0300

    First WCF example

---
+++ WCFexample/IService1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFexample
+{
+   
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IPairArihmeticService
+    {
+
+        [OperationContract]
+        Pair Add(Pair p1, Pair p2);
+
+        [OperationContract]
+        Pair Subtract(Pair p1, Pair p2);
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Pair
+    {
+        int m_first;
+        int m_second;
+        public Pair()
+        {
+            m_first = 0;
+            m_second = 0;
+        }
+        public Pair(int first, int second)
+        {
+            m_first = first;
+            m_second = second;
+        }
+
+        [DataMember]
+        public int Second
+        {
+            get { return m_second; }
+            set { m_second = value; }
+        }
+
+        [DataMember]
+        public int First
+        {
+            get { return m_first; }
+            set { m_first = value; }
+        }
+    }
+}
---
+++ WCFexample/Service1.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFexample
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class PairArihmeticService : IPairArihmeticService
+    {
+        Pair IPairArihmeticService.Add(Pair p1, Pair p2)
+        {
+            Pair result = new Pair();
+
+            result.First = p1.First + p2.First;
+            result.Second = p1.Second + p2.Second;
+
+            return result;
+        }
+
+        Pair IPairArihmeticService.Subtract(Pair p1, Pair p2)
+        {
+            Pair result = new Pair();
+
+            result.First = p1.First - p2.First;
+            result.Second = p1.Second - p2.Second;
+
+            return result;
+        }
+    }
+}
